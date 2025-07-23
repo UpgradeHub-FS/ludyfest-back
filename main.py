97f98b0a9354c4c5b621dfc63311c44cfa555919
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-23 17:29:49 +0200

    añadida ruta de eventos en main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from fastapi import FastAPI
+from routes import event_routes
+
+app = FastAPI()
+#crear la ruta para los eventos
+app.include_router(event_routes.router,
+                   prefix="/eventos",
+                   tags=["Eventos"])
--- routes/event_routes.py
+++ routes/event_routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from controllers import event_controller
 router = APIRouter()
 
+#obtener todos los eventos con GET usando la ruta del main
 @router.get('/', status_code=200)
 async def get_all():
     return await event_controller.get_event_list()
