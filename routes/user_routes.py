4052c03e4f3408106c00ff568479fa062128880f
Jonatan <jgutierreziglesias@gmail.com>
2025-07-23 16:28:47 +0200

    creada ruta post user

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,1 +1,12 @@
 from fastapi import APIRouter
+from controllers import user_controller
+from models.user_model import User, UserCreate
+
+
+
+router = APIRouter()
+
+
+@router.post('/', status_code=201)
+async def user_create(user: UserCreate):
+    return await user_controller.user_create(user)
