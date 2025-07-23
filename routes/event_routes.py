cc62c42c808aed058f34e6e327548e6fa5ec0925
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-23 17:43:38 +0200

    creado modelo event_model

--- routes/event_routes.py
+++ routes/event_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from controllers import event_controller
 router = APIRouter()
 
-#obtener todos los eventos con GET usando la ruta del main
+#obtener todos los eventos con GET (usa la ruta del main.py)
 @router.get('/', status_code=200)
 async def get_all():
     return await event_controller.get_event_list()
