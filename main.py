1fad494ee5139bcbd37ba4ac690177087517f23a
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-23 19:31:20 +0200

    I've tried so hard and got to far

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 app.include_router(event_routes.router,
                    prefix="/eventos",
                    tags=["Eventos"])
+
+@app.get("/")
+def root():
+    return {"message": "Servidor funcionando"}
--- routes/event_routes.py
+++ routes/event_routes.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 from controllers import event_controller
+
 router = APIRouter()
 
 #obtener todos los eventos con GET (usa la ruta del main.py)
+#http://localhost:8000/eventos/
 @router.get('/', status_code=200)
 async def get_all():
     return await event_controller.get_event_list()
+
