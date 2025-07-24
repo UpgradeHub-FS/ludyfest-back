77183fabac07577e28f33f9d23f34d7639306be1
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-24 13:49:50 +0200

    BBDD actualizada con datos reales

--- BBDD/ludyfest.sql
+++ BBDD/ludyfest.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ludyfest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ludyfest`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ludyfest
@@ -144,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-24 12:37:06
+-- Dump completed on 2025-07-24 13:46:47
