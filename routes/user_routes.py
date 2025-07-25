1ca340a0b9342813ef0df21193b0b34c0fa1ccff
Jonatan <jgutierreziglesias@gmail.com>
2025-07-25 21:22:54 +0200

    Funcion borrar usuario por id funcional

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -20,3 +20,8 @@ async def get_all():
 @router.get('/{user_id}', status_code=200)
 async def get_user_id(user_id: int):
     return await user_controller.get_user_id(user_id)
+
+#Creacion de delete_by_id mediante DELETE
+@router.delete('/{user_id}', status_code=200)
+async def delete_by_id(user_id: int):
+    return await user_controller.delete_by_id(user_id)
