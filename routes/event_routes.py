44e3b9ccd85ad744e421712cedf319e7d0d02e5c
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-07-26 11:34:13 +0200

    conectado back y front! Gracias Alejandro!!

--- routes/event_routes.py
+++ routes/event_routes.py
@@ -16,9 +16,9 @@ async def get_all_admin():
     return await event_controller.get_event_list_admin()
 
 #http://localhost:8000/events/{event_id}
-""" @router.get('/{event_id}', status_code=200)
+@router.get('/{event_id}', status_code=200)
 async def get_event_by_id(event_id: int):
-    return await event_controller.get_event_by_id(event_id) """
+    return await event_controller.get_event_by_id(event_id)
 
 
 
