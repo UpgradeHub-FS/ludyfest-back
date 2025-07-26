8e1626cc3afe7bbe1a22d44695513711053ebd5f
Alejandro Valdés <alejandrovaldesp86@gmail.com>
2025-07-26 13:19:17 +0200

    Corrección ortográfica

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -5,12 +5,12 @@ from controllers import auth_controllers
 
 router = APIRouter()
 
-
-@router.post("/login", status_code=201)
+#Creación de login mediante Post 
+@router.post("/login", status_code=200)
 async def login(user_login: UserLogin):
     return await auth_controllers.login(user_login)
 
-#Creacion de usuario mediante POST
+#Creación de usuario mediante POST
 @router.post('/register', status_code=201)
 async def user_create(user: UserCreate):
     return await auth_controllers.user_create(user)
